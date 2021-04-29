19bfc4654a3403ef7ce97bfde356d65652f62543
marius david <mariusdavid@laposte.net>
2021-04-29 12:35:43 +0200

    haxePackages.hxcpp: init at 4.2.1

--- pkgs/top-level/haxe-packages.nix
+++ pkgs/top-level/haxe-packages.nix
@@ -92,8 +92,8 @@ let
 
     hxcpp = buildHaxeLib rec {
       libname = "hxcpp";
-      version = "4.1.15";
-      sha256 = "1ybxcvwi4655563fjjgy6xv5c78grjxzadmi3l1ghds48k1rh50p";
+      version = "4.2.1";
+      sha256 = "sha256-F9JQMqnKgU59wdXFFZ8Rjt3u3XkwPjDeMgRSFzlaMoI=";
       postFixup = ''
         for f in $out/lib/haxe/${withCommas libname}/${withCommas version}/{,project/libs/nekoapi/}bin/Linux{,64}/*; do
           chmod +w "$f"
