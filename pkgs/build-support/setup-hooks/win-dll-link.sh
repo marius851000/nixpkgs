0acc5a8b939a7c47fad16c950781174a53f5f46b
marius david <marius@mariusdavid.fr>
2023-04-23 16:11:43 +0200

    win-dll-links: also copy dll from dependancies

--- pkgs/build-support/setup-hooks/win-dll-link.sh
+++ pkgs/build-support/setup-hooks/win-dll-link.sh
@@ -1,15 +1,48 @@
 
 fixupOutputHooks+=(_linkDLLs)
 
-# For every *.{exe,dll} in $output/bin/ we try to find all (potential)
-# transitive dependencies and symlink those DLLs into $output/bin
-# so they are found on invocation.
+addEnvHooks "$targetOffset" linkDLLGetFolders
+
+linkDLLGetFolders() {
+    addToSearchPath "LINK_DLL_FOLDERS" "$1/lib"
+    addToSearchPath "LINK_DLL_FOLDERS" "$1/bin"
+}
+
+_linkDLLs() {
+    linkDLLsInfolder "$prefix/bin"
+}
+
+# Try to links every known dependancy of exe/dll in the folder of the 1str input
+# into said folder, so they are found on invocation.
+# The optional second input is a folder where DLL present in them are ignored and not copied.
 # (DLLs are first searched in the directory of the running exe file.)
 # The links are relative, so relocating whole /nix/store won't break them.
-_linkDLLs() {
+linkDLLsInfolder() {
 (
-    if [ ! -d "$prefix/bin" ]; then exit; fi
-    cd "$prefix/bin"
+    local folder
+    folder="$1"
+    if [ ! -d "$folder" ]
+    then
+        echo "Not linking DLLs in the non-existant folder $folder"
+        return
+    fi
+    cd "$folder"
+
+    # Use associative arrays as set
+    local filesToChecks
+    local filesDone
+    declare -A filesToChecks # files that still needs to have their dependancies checked
+    declare -A filesDone # files that had their dependancies checked and who is copied to the bin folder if found
+
+    markFileAsDone() {
+        if [ "!${filesDone[$1]+a}" ]; then filesDone[$1]=a ; fi
+        if [ "${filesToChecks[$1]+a}" ]; then unset 'filesToChecks[$1]'; fi
+    }
+
+    addFileToLink() {
+        if [ "${filesDone[$1]+a}" ]; then return; fi
+        if [ "!${filesToChecks[$1]+a}" ]; then filesToChecks[$1]=a; fi
+    }
 
     # Compose path list where DLLs should be located:
     #   prefix $PATH by currently-built outputs
@@ -18,28 +51,34 @@ _linkDLLs() {
     for outName in $(getAllOutputNames); do
         addToSearchPath DLLPATH "${!outName}/bin"
     done
-    DLLPATH="$DLLPATH:$PATH"
+    DLLPATH="$DLLPATH:$LINK_DLL_FOLDERS"
 
     echo DLLPATH="'$DLLPATH'"
 
-    linkCount=0
-    # Iterate over any DLL that we depend on.
-    local dll
-    for dll in $($OBJDUMP -p *.{exe,dll} | sed -n 's/.*DLL Name: \(.*\)/\1/p' | sort -u); do
-        if [ -e "./$dll" ]; then continue; fi
-        # Locate the DLL - it should be an *executable* file on $DLLPATH.
-        local dllPath="$(PATH="$DLLPATH" type -P "$dll")"
-        if [ -z "$dllPath" ]; then continue; fi
-        # That DLL might have its own (transitive) dependencies,
-        # so add also all DLLs from its directory to be sure.
-        local dllPath2
-        for dllPath2 in "$dllPath" "$(dirname $(readlink "$dllPath" || echo "$dllPath"))"/*.dll; do
-            if [ -e ./"$(basename "$dllPath2")" ]; then continue; fi
-            CYGWIN+=\ winsymlinks:nativestrict ln -sr "$dllPath2" .
+    for peFile in *.{exe,dll}; do addFileToLink $peFile; done
+    for peFile in $LINK_DLL_TO_SKIP; do markFileAsDone $peFile; done
+
+    local linkCount=0
+    while [ ${#filesToChecks[*]} -gt 0 ]
+    do
+        listOfDlls=( "${!filesToChecks[@]}" )
+        local file=${listOfDlls[0]}
+        markFileAsDone $file
+        if [ ! -e "./$file" ]
+        then
+            local dllPath="$(PATH="$DLLPATH" type -P "$file")"
+            if [ -z "$dllPath" ]; then continue; fi
+            ln -sr "$dllPath" .
+            echo link $dllPath to $folder/$dllPath
             linkCount=$(($linkCount+1))
+        fi
+        local dep_file
+        # Look at the file’s dependancies
+        for dep_file in $($OBJDUMP -p $file | sed -n 's/.*DLL Name: \(.*\)/\1/p' | sort -u); do
+            addFileToLink $dep_file
         done
     done
-    echo "Created $linkCount DLL link(s) in $prefix/bin"
+
+    echo "Created $linkCount DLL link(s) in $folder"
 )
 }
-
